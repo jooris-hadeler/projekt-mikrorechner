2e09251ebb32a87d92adfd80c87a9b86c2eb66c0
Adrian Weber <adrian.dev@outlook.de>
2025-01-23 17:26:05 +0100

    move examples and add scripts to assemble them

---
+++ examples/comments/assemblecomments.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+cd ../..
+./assembler.sh -f ./examples/comments/comments.txt -o ./examples/comments/comments.out
---
+++ examples/includes/assembleincludes.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+cd ../..
+./assembler.sh -f ./examples/includes/test.txt -o ./examples/includes/includes.out
